0e8d59a226b2ae8d36134fdff2cd54942a923265
Alina Tuktarova <alinka.tuk.tuk@gmail.com>
2021-08-26 00:48:50 +0300

    Добавление фамилии в header

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import menuState from './menuReducer';
+import valuesState from './valuesReducer';
 
-const rootReducers = () => combineReducers({ menuState })
+const rootReducers = () => combineReducers({ menuState, valuesState })
 
 export default rootReducers;
