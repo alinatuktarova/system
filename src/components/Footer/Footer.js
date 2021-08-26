5f3319503c2b5730e1fe942813e4c41c1df091a7
Alina Tuktarova <alinka.tuk.tuk@gmail.com>
2021-08-26 14:31:46 +0300

    fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,15 +2,9 @@ import React from 'react'
 import styles from './Footer.module.scss'
 
 const Footer = () => (
-  <>
-    <div className={styles.footer}>
-      <h2 className={styles.footer__title}>Название</h2>
-      <div className={styles.footer__block_text}>
-        <span>Помощь</span>
-        <span>Контакты</span>
-      </div>
-    </div>
-  </>
+  <div className={styles.footer}>
+    <h2 className={styles.footer__title}>©Учет успеваемости 2021</h2>
+  </div>
 )
 
 export default Footer
